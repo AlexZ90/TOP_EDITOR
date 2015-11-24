86ccf9dda07751b80faee426a56f7da3c23e86ad
chubrik <spbrothers@inbox.ru>
2015-11-25 00:17:05 +0300

    Начал переписывать класс соединения.

---
+++ Sources/Connection_.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TopEditor
+{
+  class Connection
+  {
+
+    public string Name;
+    public List<Instance> listOfInsts;
+    public List<Port> listOfPorts;
+    public int external;
+
+
+    public Connection(string cName, int ext)
+    {
+      Name = cName;
+      listOfInsts = new List<Instance>();
+      listOfPorts = new List<Port>();
+      external = ext;
+    }
+
+    public void addInstPort (Instance inst, Port port)
+    {
+      listOfInsts.Add(inst);
+      listOfPorts.Add(port);
+    }
+
+    public void removeInstPort(Instance inst, Port port)
+    {
+
+      Instance instance;
+
+    }
+
+
+  }
+}
