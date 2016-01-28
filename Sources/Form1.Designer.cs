15ef045e63aa8415a349b415cbb7b971c66b6cd2
chubrik90 <chubrik90@users.noreply.github.com>
2016-01-28 14:06:38 +0300

    Используя свойство Anchor нижнего текстбокса, реализовал возможность изменения размера текстбокса.

--- Sources/Form1.Designer.cs
+++ Sources/Form1.Designer.cs
@@ -351,6 +351,9 @@
             // 
             // outputRTB
             // 
+            this.outputRTB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.outputRTB.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.outputRTB.Location = new System.Drawing.Point(227, 595);
             this.outputRTB.Name = "outputRTB";
